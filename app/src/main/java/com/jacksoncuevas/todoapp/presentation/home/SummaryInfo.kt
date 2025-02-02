98b89dba3aec18a9fa041ce64a615da08a1fe198
Jackson Cuevas <jackson.cuevas@hotmail.com>
2025-02-02 12:13:59 -0400

    Layouts: Column, Row y Box

---
+++ app/src/main/java/com/jacksoncuevas/todoapp/presentation/home/SummaryInfo.kt
@@ -0,0 +1,46 @@
+package com.jacksoncuevas.todoapp.presentation.home
+
+import androidx.compose.foundation.content.MediaType.Companion.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SummaryInfo(
+    modifier: Modifier = Modifier,
+    date: String = "March 9, 2024",
+    taskSummary: String = "5, incomplete, 5 completed"
+) {
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(
+            text = date,
+            style = MaterialTheme.typography.headlineLarge,
+            color = MaterialTheme.colorScheme.onBackground,
+            fontWeight = FontWeight.Bold
+        )
+
+        Text(
+            text = taskSummary,
+            style = MaterialTheme.typography.titleSmall,
+            color = MaterialTheme.colorScheme.onSurface,
+        )
+
+    }
+}
+
+@Preview(
+    showBackground = true,
+    name = "Summaryn Info Preview"
+)
+@Composable
+fun PreviewSummaryInfo() {
+    MaterialTheme {
+        SummaryInfo()
+    }
+}
